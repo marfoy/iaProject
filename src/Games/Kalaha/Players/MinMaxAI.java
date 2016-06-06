bef2a640b2122749ebafda444f24bf5d892acc04
Clément Tamines <clement.tamines@gmail.com>
2016-06-06 15:18:04 +0200

    This class now creates a heuristic (which can depend on the player stored un string s) and creates a minMax instance which computes the best move using the heuristic and the minmax algorithm

--- src/Games/Kalaha/Players/MinMaxAI.java
+++ src/Games/Kalaha/Players/MinMaxAI.java
@@ -6,87 +6,14 @@ import Games.Kalaha.Players.AI.Minimax;
 import Games.Kalaha.Players.AI.Minimax2;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by mrt on 19/04/16.
  */
 
 public class MinMaxAI extends Player{
-    /*
-    private Heuristic heuristic;
-    public MinMaxAI(Heuristic heur){
-        this.heuristic = heur;
-    }
 
-    @Override
-    public Move pickMove(String s) {
-        return minMaxDecision(s);
-    }
-    public Move minMaxDecision(String avatar){
-        int maxUtility = 0;
-        int tempUtility;
-        int action = 0;
-        for (int i = 0; i < board.getLength(); ++i) {
-            tempUtility = minValue(result(board,new Move(i)),avatar);
-            if (board.getPlayer(i).equals(avatar) && !board.isKalaha(i) && board.getPieceAt(i) > 0 && tempUtility >= maxUtility) {
-                maxUtility = tempUtility;
-                action = i;
-            }
-        }
-        return new Move(action);
-    }
-
-    private int maxValue(Board result,String avatar) {
-        if(terminalTest(result,avatar))
-            return heuristic.compute(result);
-        else{
-            int v = Integer.MIN_VALUE;
-            ArrayList<Integer> actions = actionPossible(result,avatar);
-            for(Integer action : actions){
-                v = Math.max(v,minValue(result(board,new Move(action)),avatar));
-            }
-            return v;
-        }
-    }
-    private int minValue(Board result,String avatar) {
-        if(terminalTest(result,avatar))
-            return heuristic.compute(result);
-        else{
-            int v = Integer.MAX_VALUE;
-            ArrayList<Integer> actions = actionPossible(result,avatar);
-            for(Integer action : actions){
-                v = Math.min(v,maxValue(result(board,new Move(action)),avatar));
-            }
-            return v;
-        }
-    }
-
-    private ArrayList<Integer> actionPossible(Board result,String avatar) {
-        ArrayList<Integer> possibleMoves = new ArrayList<>();
-        for (int i = 0; i < result.getLength(); ++i) {
-            if (result.getPlayer(i).equals(avatar) && !result.isKalaha(i) && result.getPieceAt(i) > 0 ) {
-                possibleMoves.add(i);
-            }
-        }
-        return possibleMoves;
-    }
-
-    private boolean terminalTest(Board state,String avatar){
-        for (int i = 0; i < state.getLength(); ++i) {
-            if (!board.getPlayer(i).equals(avatar) && !board.isKalaha(i) && board.getPieceAt(i) > 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public Board result(Board board, Move action){
-        ///TODO Need to implement apply(move) method who already exist but this method take game parameter,that we don't need here
-        //Game copyGame = new Game(game.getBoardClone(),game.getLeftTokensGrantee(),game.getEmptyCapture(),game.getPlayers());
-        //action.apply(copyGame);
-        return board;
-    }
-*/
     public int i = 0;
     @Override
     public Move pickMove(String s) {
@@ -94,8 +21,17 @@ public class MinMaxAI extends Player{
         //We extend core player which knows the board
         System.out.println("Nom IA : "+s+" joue son "+i+ "  ème coup");
         i++;
-        Heuristic heuristic = new Minimax(s, players, 6, leftTokensGrantee, emptyCapture);
-        return new Move(heuristic.compute(board));
+        class Minimizer implements Heuristic {
+
+            @Override
+            public double compute(Board board) {
+               /*TODO Implement heuristic 1 here*/
+                return 0;
+            }
+        }
+        Heuristic minimizer = new Minimizer();
+        Minimax minmax = new Minimax(s, players, 10 ,minimizer,leftTokensGrantee, emptyCapture);
+        return new Move(minmax.bestMove(board));
     }
 
 }
