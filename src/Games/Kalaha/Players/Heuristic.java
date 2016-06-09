ffa92e51390a0222e93967e624be2b63e96cffbb
Clément Tamines <clement.tamines@gmail.com>
2016-06-09 23:49:13 +0200

    Comments

--- src/Games/Kalaha/Players/Heuristic.java
+++ src/Games/Kalaha/Players/Heuristic.java
@@ -1,10 +1,10 @@
 package Games.Kalaha.Players;
 
 import Games.Kalaha.Boards.Board;
-import Games.Kalaha.Move;
 
 /**
- * Created by mrt on 19/04/16.
+ * This interface is used to represent a heuristic. We added player to compute method in order to know which player
+ * we are computing the heuristic for (usefull for MaxN)
  */
 public interface Heuristic {
      double compute(Board board, String player);
