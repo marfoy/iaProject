0e9a4f00f4a96b873ec7b0b6d1d3bfc6428d4b08
Imperatore <martin.lempereur@student.umons.ac.be>
2016-06-10 00:21:52 +0200

    Heuristic correction

--- src/Games/Kalaha/Players/TLMinReserveAI.java
+++ src/Games/Kalaha/Players/TLMinReserveAI.java
@@ -22,13 +22,13 @@ public class TLMinReserveAI extends Player{
 
             @Override
             public double compute(Board board, String player) {
-		int maxReserve = 0;
+		int minKalaha = 0;
 		for (int i = 0; i < board.getLength();++i){
 		    if(board.isKalaha(i) && !board.getPlayer(i).equals(player)){
-		       maxReserve = Math.max(maxReserve,board.getPieceAt(i));
+		       minKalaha = board.getPieceAt(i);
 		    }
 		}
-		return  maxReserve;
+		return  minKalaha;
             }
         }
         Heuristic minimizer = new Minimizer();
