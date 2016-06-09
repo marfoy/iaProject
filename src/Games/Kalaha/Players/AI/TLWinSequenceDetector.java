3d0ade085f92b65bea3e44b60765a921e0031910
Clément Tamines <clement.tamines@gmail.com>
2016-06-10 01:44:02 +0200

    Added tests to winsequence

--- src/Games/Kalaha/Players/AI/TLWinSequenceDetector.java
+++ src/Games/Kalaha/Players/AI/TLWinSequenceDetector.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 /**
  * Created by Clement
  * This AI returns the move that needs to be played in order to continue an uninterrupted winning sequence of actions
- * Move can be -infinity if no such move exists
+ * Move can be -infinity if no such move exists. The main of this class contains the examples from the assignment as
+ * well as some examples that will fail. We see that all tests pass.
  */
 public class TLWinSequenceDetector {
 
@@ -78,5 +79,80 @@ public class TLWinSequenceDetector {
         //If we get here, no winning move (at this depth or further down) has been found. The move played before didn't
         //Allow us to win
         return Double.NEGATIVE_INFINITY;
+    }
+
+    public static void main(String[] args) {
+        //Tests from the example of the assignment
+        ArrayList<Integer> test1 = new ArrayList<>();
+        test1.add(0);
+        test1.add(0);
+        test1.add(1);
+        ArrayList<Integer> test2 = new ArrayList<>();
+        test2.add(0);
+        test2.add(2);
+        test2.add(0);
+        ArrayList<Integer> test3 = new ArrayList<>();
+        test3.add(0);
+        test3.add(2);
+        test3.add(1);
+        ArrayList<Integer> test4 = new ArrayList<>();
+        test4.add(3);
+        test4.add(1);
+        test4.add(0);
+        ArrayList<Integer> test5 = new ArrayList<>();
+        test5.add(3);
+        test5.add(1);
+        test5.add(1);
+
+        //Tests that will fail
+        ArrayList<Integer> test6 = new ArrayList<>();
+        test6.add(4);
+        test6.add(0);
+        test6.add(0);
+
+        ArrayList<Integer> test7 = new ArrayList<>();
+        test7.add(0);
+        test7.add(3);
+        test7.add(0);
+
+        ArrayList<Integer> test8 = new ArrayList<>();
+        test8.add(0);
+        test8.add(0);
+        test8.add(2);
+
+        ArrayList<Integer> test9 = new ArrayList<>();
+        test9.add(4);
+        test9.add(2);
+        test9.add(1);
+
+        System.out.println("Test 1 :");
+        System.out.println(test1);
+        System.out.println(winSequence(test1));
+        System.out.println("Test 2 :");
+        System.out.println(test2);
+        System.out.println(winSequence(test2));
+        System.out.println("Test 3 :");
+        System.out.println(test3);
+        System.out.println(winSequence(test3));
+        System.out.println("Test 4 :");
+        System.out.println(test4);
+        System.out.println(winSequence(test4));
+        System.out.println("Test 5 :");
+        System.out.println(test5);
+        System.out.println(winSequence(test5));
+        System.out.println("Test 6 :");
+        System.out.println(test6);
+        System.out.println(winSequence(test6));
+        System.out.println("Test 7 :");
+        System.out.println(test7);
+        System.out.println(winSequence(test7));
+        System.out.println("Test 8 :");
+        System.out.println(test8);
+        System.out.println(winSequence(test8));
+        System.out.println("Test 9 :");
+        System.out.println(test9);
+        System.out.println(winSequence(test9));
+
+        //All tests are working as expected
     }
 }
